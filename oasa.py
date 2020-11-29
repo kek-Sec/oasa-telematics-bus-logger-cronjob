b41a048c316a1c8142de3927d1c8d0831a0ef693
kek-Sec <zarkosweg@gmail.com>
2020-11-29 19:51:16 +0200

    added getBuses

--- oasa.py
+++ oasa.py
@@ -8,53 +8,63 @@ import signal
 
 loop = asyncio.get_event_loop()
 client = aiohttp.ClientSession(loop=loop)
-LineCodesList = {} #contains all available LineCodes
-RouteCodesList = {} #contains all corresponding RouteCodes to the LineCodesList
-BusesList = {}#contains all buses
+LineCodesList = {}  # contains all available LineCodes
+RouteCodesList = {}  # contains all corresponding RouteCodes to the LineCodesList
+BusesList = {}  # contains all buses
+
 
 async def get_json(client, url):
     async with client.get(url) as response:
         assert response.status == 200
         return await response.read()
 
+
 async def getLineCodes(client):
     to_return = {}
     c = 0
-    res = await get_json(client,"http://telematics.oasa.gr/api/?act=webGetLines")
+    res = await get_json(client, "http://telematics.oasa.gr/api/?act=webGetLines")
     items = json.loads(res)
     for item in items:
-        to_return[c] = item['LineCode']
-        c+=1
+        to_return[c] = item["LineCode"]
+        c += 1
     return to_return
 
-async def getRouteCodes(linesList,client):
+
+async def getRouteCodes(linesList, client):
     to_return = {}
     c = 0
     for i in range(len(linesList)):
-        res = await get_json(client,"http://telematics.oasa.gr/api/?act=getRoutesForLine&p1=" + str(linesList[i]))
+        res = await get_json(
+            client,
+            "http://telematics.oasa.gr/api/?act=getRoutesForLine&p1="
+            + str(linesList[i]),
+        )
         items = json.loads(res)
         d = 0
-        if(items):
+        if items:
             for item in items:
-                if(d > 0):
-                    to_return[c] = to_return[c] + ',' + item['route_code']
+                if d > 0:
+                    to_return[c] = to_return[c] + "," + item["route_code"]
                 else:
-                    to_return[c] = item['route_code']
-                d+=1
-            c+=1
+                    to_return[c] = item["route_code"]
+                d += 1
+            c += 1
     return to_return
 
-async def getBuses(routesList,client):
+
+async def getBuses(routesList, client):
     to_return = {}
     c = 0
     for i in range(len(routesList)):
-        x = routesList[i].split(',')
+        x = routesList[i].split(",")
         for route in x:
-            res = await get_json(client,"http://telematics.oasa.gr/api/?act=getBusLocation&p1=" + route)
+            res = await get_json(
+                client, "http://telematics.oasa.gr/api/?act=getBusLocation&p1=" + route
+            )
             items = json.loads(res)
             for item in items:
-                to_return[c] = item
-                c+=1
+                to_return[c] = item["VEH_NO"]
+                c += 1
     return to_return
 
 
@@ -63,14 +73,17 @@ def signal_handler(signal, frame):
     client.close()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 async def init(client):
     LineCodesList = await getLineCodes(client)
     print(LineCodesList)
-    RouteCodesList = await getRouteCodes(LineCodesList,client)
+    RouteCodesList = await getRouteCodes(LineCodesList, client)
     print(RouteCodesList)
-    BusesList = await getBuses(RouteCodesList,client)
+    BusesList = await getBuses(RouteCodesList, client)
+    print(BusesList)
 
 
 # 0- Entry point -0
